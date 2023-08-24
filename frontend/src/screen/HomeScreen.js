e045254ada56e6fca20f74abf84d2e0f2718dfa0
Gaytri Dodiya <dodiyadsvv@gmail.com>
2023-08-24 17:12:04 +0530

    initiate homeScreen

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import RoomConfigutore from '../component/RoomConfigutore';
 import DisplayConfigutore from '../component/DisplayConfigutore';
-
+import { Col, Row, Image } from 'react-bootstrap';
 export default function HomeScreen() {
   return (
-    <div>
-      <RoomConfigutore />
-      <DisplayConfigutore />
+    <div className='bg-dark HomeContainer container-fluid'>
+      <Row className="d-flex justify-content-center" >
+        <Image src="/assets/logo.png" className="homeLogo" alt="auruvisiton" />
+      </Row>
+      <Row>
+        <Col md={2} className="borders mainCol">
+          <Row>
+            <Col>
+              <RoomConfigutore />
+            </Col> </Row>
+          <Row>
+            <Col>
+              <DisplayConfigutore />
+            </Col>
+          </Row>
+        </Col>
+
+        <Col md={8} className="borders mainCol">center</Col>
+        <Col md={2} className="borders mainCol">
+          Right
+        </Col>
+      </Row>
+
+
     </div>
   );
 }
