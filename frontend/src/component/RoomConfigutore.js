a7fd7a36df93e361218ca93de6a6a9d25705873b
Dipak <deepraj41352@gmail.com>
2023-08-24 18:55:39 +0530

    room type pop

--- frontend/src/component/RoomConfigutore.js
+++ frontend/src/component/RoomConfigutore.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import SelectYourEvt from './SelectYourEvt';
 
 export default function RoomConfigutore() {
-  return <div>RoomConfigutore</div>;
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Row>
+        <Row>
+          <Col>
+            <span className="roomheader">ROOM CONFIGURATOR</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="primary" onClick={handleShow}>
+              Launch demo modal
+            </Button>
+          </Col>
+          <SelectYourEvt show={show} setShow={setShow} />
+        </Row>
+      </Row>
+    </>
+  );
 }
