b838dd49342d883ba10bbfa38931ccf00a9f21e7
Dipak <deepraj41352@gmail.com>
2023-08-24 16:20:32 +0530

    install react

---
+++ frontend/src/screen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoomConfigutore from '../component/RoomConfigutore';
+import DisplayConfigutore from '../component/DisplayConfigutore';
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <RoomConfigutore />
+      <DisplayConfigutore />
+    </div>
+  );
+}
